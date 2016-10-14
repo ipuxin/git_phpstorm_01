9fe5a86209b11d119afd79b8fc980e3d21c3b3fb
ipuxin <ipuxin@qq.com>
2016-10-14 15:01:21 +0800

    变小后

--- index.php
+++ index.php
@@ -8,15 +8,16 @@
 ?>
 <link rel="stylesheet" href="css/main.css">
 <div class="colorSpan">
-    This is doudou! 我家的 引入新成员后<br>
-    增加icon!
     <img src="images/gom_icon.png" alt="git">
+    <span>This is doudou! 我家的 引入新成员后</span><br>
+    <span>增加icon!</span>
+
 </div>
 
 <div class="colorSpan">
-    要发布啦<br>
-    增加icon!
     <img src="images/gom_icon.png" alt="git">
+    <span>要发布啦</span><br>
+
 </div>
 
 <!--这次增加了两张图,在增加两张图的分支上-->
